de7a0ccc5a78dde15be0013a56fa48487d0c9580
Swen <knight12345@mail.ru>
2024-05-02 20:36:41 +0300

    sql script

---
+++ MindboxTask/SQLScript.cs
@@ -0,0 +1,22 @@
+﻿
+//CREATE TABLE Product
+//(
+//    Id INT IDENTITY PRIMARY KEY,
+//    Name NVARCHAR(30) NOT NULL
+//);
+//CREATE TABLE Category
+//(
+//    Id INT IDENTITY PRIMARY KEY,
+//    Name NVARCHAR(30) NOT NULL
+//);
+//CREATE TABLE ProductCategory
+//(
+//    Id INT IDENTITY PRIMARY KEY,
+//    ProductId INT NOT NULL REFERENCES Product(Id) ON DELETE CASCADE,
+//    CategoryId INT NOT NULL REFERENCES Category(Id) ON DELETE CASCADE
+//);
+
+//SELECT Product.Name, Category.Name
+//FROM Product, Category, ProductCategory
+//WHERE ProductCategory.ProductId = Product.Id AND ProductCategory.CategoryId = Category.Id
+//ORDER BY Product.Name
